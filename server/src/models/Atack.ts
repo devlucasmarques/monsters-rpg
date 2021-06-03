2dfeb85f812fcf8aede0331420fb50eea170d5e7
Lucas dev <devlucasmarques@gmail.com>
2021-06-03 12:04:04 -0300

    review atack CRUD

--- server/src/models/Atack.ts
+++ server/src/models/Atack.ts
@@ -1,15 +1,18 @@
 import mongoose, { Schema } from 'mongoose';
 
-export const Atack = new Schema({
-  id: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: false
+export const Atack = new Schema(
+  {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: false
+    },
+    name: String,
+    damage: Number,
+    energy: Number,
+    type: Number
   },
-  name: String,
-  damage: Number,
-  energy: Number,
-  type: Number
-});
+  { timestamps: true }
+);
 
 const AtackModel = mongoose.model('AtackModel', Atack);
 
